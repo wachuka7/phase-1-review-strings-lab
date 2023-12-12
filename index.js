82818369b8d918c0a59173ca78d668a4366b6545
wachuka7 <rachaelwachuka7@gmail.com>
2023-12-12 21:58:03 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Rachael Wachuka';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = ` Welcome, ${currentUser.slice (0, 1)}!`
